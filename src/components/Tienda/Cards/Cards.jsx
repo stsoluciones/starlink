e4beb3d8886874993b7fa3d426e87621b66c2f96
gtorresgrau <gonzalotorresgrau@gmail.com>
2025-05-20 23:34:40 -0300

    lupa

--- src/components/Tienda/Cards/Cards.jsx
+++ src/components/Tienda/Cards/Cards.jsx
@@ -52,9 +52,9 @@ const Cards = ({
                 ) : (
                 <li className="w-full md:w-1/2 lg:w-1/3"> {/* Ocupa todo el ancho en pantallas pequeñas, la mitad en medianas y un tercio en grandes */}
                     <div className="flex flex-col justify-center items-center mx-4 mt-6 px-20 py-10 border rounded-md shadow-lg bg-gray-200">
-                      <svg fill="#000000" className="w-10 h-10" height="800px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" >
-                        <path d="m9 4.45-2 2-2-2-1 1 2 2-2 2 1 1 2-2 2 2 2 1-1-2-2 2-2zm2.77 6.63c.77-1.01 1.23-2.27 1.23-3.63 0-3.31-2.69-6-6-6s-6 2.69-6 6 2.69 6 6 6c1.37 0 2.63-.46 3.64-1.24l2.79 2.79 1.13-1.13zm-4.87.76c-2.48 0-4.49-2.02-4.49-4.5s2.02-4.5 4.49-4.5 4.5 2.02 4.5 4.5-2.03 4.5-4.5 4.5z" />
-                      </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" className="w-10 h-10" viewBox="0 0 24 24">
+                          <path d="M10 2a8 8 0 105.293 14.293l4.707 4.707 1.414-1.414-4.707-4.707A8 8 0 0010 2zm3.707 10.707L12 11.414l-1.707 1.707-1.414-1.414L10.586 10 8.879 8.293l1.414-1.414L12 8.586l1.707-1.707 1.414 1.414L13.414 10l1.707 1.707-1.414 1.414z"/>
+                        </svg>
                         <p className="text-gray-800 font-semibold">No se encontraron productos que coincidan con tu búsqueda.</p>
                     </div>
                 </li>
