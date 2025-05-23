6a99210671bc1cb0cfa740df96bf6b6e58c41ba5
gtorresgrau <gonzalotorresgrau@gmail.com>
2025-05-22 22:17:56 -0300

    metadata

--- src/lib/metadata.js
+++ src/lib/metadata.js
@@ -3,7 +3,6 @@ export const defaultMetadata = {
   title: 'Starlink Mini | Fuente elevadora de tension - SLS',
   description: 'Adquirí la nueva fuente elevadora de tension para Starlink Mini: internet satelital portátil, compacto y potente. Ideal para viajes, zonas rurales o lugares sin conexión. Envíos a todo el país.',
   keywords: 'Starlink Mini, internet satelital, internet portátil, Starlink Argentina, conexión rural, SLS, WiFi portátil, antena Starlink, conexion estable, internet de alta velocidad, Starlink, internet satelital, Starlink Mini, Starlink Argentina, Starlink SLS, Starlink Soluciones, Starlink portatil, internet rural, internet en movimiento, internet en viajes, internet en zonas rurales, internet sin limite de datos, fuente elevadora de tension, Starlink Mini fuente elevadora de tension, Starlink Mini Argentina, Starlink Mini SLS, Starlink Mini Soluciones, Starlink Mini portatil, Starlink Mini internet rural, Starlink Mini internet en movimiento, Starlink Mini internet en viajes, Starlink Mini internet en zonas rurales, Starlink Mini internet sin limite de datos',
-  viewport: 'width=device-width, initial-scale=1',
   charSet: 'UTF-8',
   icons: '/favicon.ico',
   manifest: '/manifest.json',
@@ -13,18 +12,18 @@ export const defaultMetadata = {
   openGraph: {
     title: 'Starlink Mini | Internet satelital portátil de alta velocidad - SLS',
     description: 'Adquirí el nuevo Starlink Mini: internet satelital portátil, compacto y potente. Ideal para viajes, zonas rurales o lugares sin conexión. Envíos a todo el país.',
-    type: 'product',
-    url: 'https://starlinksoluciones.com/productos/StarLinkmini',
-    image: 'https://starlinksoluciones.com/img/starlink-mini.webp',
+    type: 'website',
+    url: 'https://starlinksoluciones.com/',
+    image: 'https://starlinksoluciones.com/logos/logoSLS.webp',
   },
   twitter: {
     card: 'summary_large_image',
     site: '@starlinksoluciones',
     title: 'Starlink Mini | Internet satelital portátil de alta velocidad - SLS',
     description: 'Adquirí el nuevo Starlink Mini: internet satelital portátil, compacto y potente. Ideal para viajes, zonas rurales o lugares sin conexión. Envíos a todo el país.',
-    images: [{ url: 'https://starlinksoluciones.com/img/starlink-mini.webp' }],
+    images: [{ url: 'https://starlinksoluciones.com/logos/logoSLS.webp' }],
   },
   alternates: {
-    canonical: 'https://starlinksoluciones.com/productos/StarLinkmini',
+    canonical: 'https://starlinksoluciones.com/',
   }
 };
